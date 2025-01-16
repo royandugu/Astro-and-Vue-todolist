1fa23bbb3839fa5df9378f336f6659f4677644c5
Royan Dugu (OMG) <royan.dugu@omnicommediagroup.com>
2025-01-16 09:36:48 +0545

    Todo vue

---
+++ src/components/Vue/Todo.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+<template>
+    <section class="todo-list">
+        <div class="todo-flex">
+            <input type="checkbox"/>
+            <h1> Task one </h1>
+        </div>
+        <div class="todo-flex">
+            <input type="checkbox"/>
+            <h1> Task one </h1>
+        </div>
+        <div class="todo-flex">
+            <input type="checkbox"/>
+            <h1> Task one </h1>
+        </div>
+    </section>
+</template>
