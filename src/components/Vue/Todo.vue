d78fe3edaa7e82c0596e43209708f6d5862f8c15
Royan Dugu (OMG) <royan.dugu@omnicommediagroup.com>
2025-01-16 12:19:13 +0545

    Todo list

--- src/components/Vue/Todo.vue
+++ src/components/Vue/Todo.vue
@@ -1,19 +1,30 @@
-<script>
+<script setup>
+import { todoList } from '../../data/todoList';
+import { ref } from 'vue';
 
+const listRef = ref(todoList);
+    
+const taskDone = (item) =>{
+    listRef.value = listRef.value.filter((list) => list !== item);
+}
 </script>
 <template>
     <section class="todo-list">
-        <div class="todo-flex">
-            <input type="checkbox"/>
-            <h1> Task one </h1>
-        </div>
-        <div class="todo-flex">
-            <input type="checkbox"/>
-            <h1> Task one </h1>
-        </div>
-        <div class="todo-flex">
-            <input type="checkbox"/>
-            <h1> Task one </h1>
+        <div class="container">
+            <div class="todo-flex" v-for="(item) in listRef" :key="item.id">
+                <input type="checkbox" @click="taskDone(item)"/>
+                <h1> {{ item.taskName }} </h1>
+            </div>
         </div>
     </section>
 </template>
+<style>
+.todo-flex {
+    display: flex;
+    gap: 12px;
+}
+
+input {
+    cursor: pointer;
+}
+</style>
---
+++ src/data/todoList.ts
@@ -0,0 +1,22 @@
+export const todoList=[
+    {
+        id:1,
+        taskName:"Task a",
+        status:"pending"
+    },
+    {
+        id:2,
+        taskName:"Task b",
+        status:"pending"
+    },
+    {
+        id:3,
+        taskName:"Task c",
+        status:"pending"
+    },
+    {
+        id:4,
+        taskName:"Task d",
+        status:"pending"
+    }
+]
