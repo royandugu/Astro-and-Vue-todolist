17bab8301d54f5930cc11ca5048e58d3f925ccc2
Royan Dugu (OMG) <royan.dugu@omnicommediagroup.com>
2025-01-21 14:01:54 +0545

    Completed task list

--- src/components/Vue/Todo.vue
+++ src/components/Vue/Todo.vue
@@ -3,9 +3,16 @@ import { todoList } from '../../data/todoList';
 import { ref } from 'vue';
 
 const listRef = ref(todoList);
+const tickedListsRef = ref([]);
     
 const taskDone = (item) =>{
     listRef.value = listRef.value.filter((list) => list !== item);
+    tickedListsRef.value = [...tickedListsRef.value,item];
+}
+
+const taskUndone = (item) =>{
+    tickedListsRef.value = tickedListsRef.value.filter((list) => list !== item);
+    listRef.value = [...listRef.value,item];
 }
 </script>
 <template>
@@ -15,10 +22,16 @@ const taskDone = (item) =>{
                 <input type="checkbox" @click="taskDone(item)"/>
                 <h1> {{ item.taskName }} </h1>
             </div>
+            <h1> This is the completed tasks : </h1>
+            <div class="todo-flex" v-for="(item) in tickedListsRef" :key="item.id">
+                <input type="checkbox" @click="taskUndone(item)"/>
+                <h1> {{item.taskName}} </h1>
+            </div>
         </div>
     </section>
 </template>
 <style>
+
 .todo-flex {
     display: flex;
     gap: 12px;
